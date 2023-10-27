d580d778df4d3442939ab2a2d86d5b070bb7105c
DeusProtivogas <kirill2689@gmail.com>
2023-10-27 22:37:41 +0000

    Fixes

--- main.py
+++ main.py
@@ -32,35 +32,32 @@ params_Cher = {"n": "",
               "lang": "ru",
 }
 
-
-url_Bad = "https://httpstat.us/"
-params_Bad = {"n": "", 
-            "T": "", 
-            "u": "", 
-            "q": "", 
-            "lang": "en",
-}
-
-
 locations = [(url_SF, params_SF), 
              (url_Lon, params_Lon), 
-             # (url_Bad, params_Bad), 
              (url_Sher, params_Sher), 
              (url_Cher, params_Cher)
             ]
 
-def get_weather_forecast(url, params):
-    try:
-        response = requests.get(url, params=params)
-        response.raise_for_status()
-    
-    except (requests.exceptions.ConnectionError, requests.exceptions.HTTPError):
-        return None
-    return response.text
 
-for loc, params in locations:
-    result = get_weather_forecast(loc, params)
-    if result is None:
-        print("Error in URL: ", loc)
-    else:
-        print(result)
+def get_weather_forecast(url, params):
+        response = requests.get(url, params=params)
+        # print(response)
+        response.raise_for_status()
+        return response.text
+
+def main():
+    for loc, params in locations:
+        try:
+            # print(loc, "  :  ", params)
+            result = get_weather_forecast(loc, params)
+            # print(result)
+            if result is None:
+                print (f"Error in URL: {loc}")
+            else:
+                print (result)
+        except (requests.exceptions.ConnectionError, requests.exceptions.HTTPError) as e:
+            print (e) 
+        # print ( result.text )
+
+if __name__ == "__main__":
+    main()
